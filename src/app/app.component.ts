3a4268c46e4d825ea6ec0de34ac9dd7ca49d941d
Nueva Cuenta <nuevacuenta@Aniass-MacBook-Pro.local>
2024-10-20 18:32:52 -0500

    componentes, registrar, home y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CommonModule], // Agregar CommonModule aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ProyectoTics';
+  showHeaderFooter = true; // Controla la visibilidad del header y footer
+
+  constructor(private router: Router) {
+    // Observa los cambios de navegación
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // Comprueba si la ruta actual es '/register'
+        this.showHeaderFooter = event.url !== '/register';
+      });
+  }
 }
