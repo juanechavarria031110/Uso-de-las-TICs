fe8112cf1d881fa6e15275405845280566c6927a
juanechavarria031110 <jechavarrialoaiza@gmail.com>
2024-10-22 14:31:53 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  // Aquí iría la lógica del componente
 }
+
