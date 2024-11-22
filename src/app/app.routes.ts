ddeb57962ad68c20240d685715b615c7db53591e
Cristina Escobar <mces11.12@gmail.com>
2024-11-22 00:45:36 -0500

    act intf admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router'; 
 import { LoginComponent } from './login/login.component'; 
 import { RegisterComponent } from './register/register.component'; 
-import { HomeComponent } from './home/home.component'; // Asegúrate de importar el componente Home 
- 
+import { HomeComponent } from './home/home.component'; 
+import { AdminComponent } from './admin/admin.component'; // Asegúrate de haber importado este componente correctamente
+
 export const routes: Routes = [ 
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige a Home como ruta inicial 
   { path: 'home', component: HomeComponent }, // Ruta para el componente de Home 
   { path: 'login', component: LoginComponent }, // Ruta para el componente de login 
-  { path: 'register', component: RegisterComponent } // Ruta para el componente de registro 
+  { path: 'register', component: RegisterComponent }, // Ruta para el componente de registro
+  { path: 'admin', component: AdminComponent }, // Ruta para el componente de administración
 ];
